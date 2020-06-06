4abcec01378f8c60747a00b7495f1cf69874b951
rpineda <roberto.pineda@pachaqtec.edu.pe>
2020-06-05 21:46:26 -0500

    Clase de la semana 2 sesion 3

---
+++ Semana2Sesion3/virtual/init.py
@@ -0,0 +1,15 @@
+import os
+import array
+
+import os, array
+
+print("hola desde python")
+
+if(1==1):
+    print("verdad")
+else:
+    print("mentira 12345678901234567880123445678901234567"
+        +"8901234567891234567890000000")
+
+print ( "Evita los espacios" )
+print("asi es mejor")
