7648aed6dd2a97488421a52ed56d0e7fc657eba3
sperezt <sergio.perez.tafur@gmail.com>
2020-06-12 21:24:56 -0500

    practicando

---
+++ Semana3Sesion2/milla.py
@@ -0,0 +1,7 @@
+print("Este programa calcula mpg.")
+ 
+millas_recorridas = float(input("Introduce las millas recorridas: "))
+galones_usados = float(input("Introduce los galones usados: "))
+
+mpg = millas_recorridas / galones_usados
+print ("Millas por galon:",mpg)
