1e6ad0ab6c40acbb8205b667d08c55e33c2dd55e
PolCode33 <hvf01010@gmail.com>
2020-06-14 23:50:44 -0500

    Semana3Hackathon

---
+++ Semana3Hackaton/Hvasquez/ProgramaInventarioProduc.py
@@ -0,0 +1,180 @@
+#Programa de Inventario
+#Acciones:
+#1.- Agregar Productos
+# 1.1.-Menu para agregar productos o salir
+#2.- Quitar Producto
+# 2.1.-Menu para quitar producto o salir
+#3.- Inventario
+# 3.1.-Menu para Ejecutar el Inventario o salir
+
+print (",.,.,.,...,... .0ZMMBMEMMJv777vir7UkOOMOMMMOMGMOMMOZOEOB: ..............,..........................")
+print ("               :ZX8FjX1jUYii::::,::r7LL15SFk111N8BGEGOOBX                       ...................")                       
+print (",...,........  10qS::iLLriii::i:::::::iir7YLLvYuGOS2FXMB@O  .......................................")
+print ("...,......... ;OM@r:::iii:,:,::::iii:iiii;irr7rvUY77r7JMB@7  ......................................")
+print ("..............F8B5i:i:iiiii:::i:i:iiii;ir;rrrr777r777rij@@@   ......... ...........................")
+print ("............ :EOP7:iii:i:iii:::::iiii;iiirrrr77v777vr7r70@BS   . ... ... ..........................")
+print ("............ :GOkriiiii:i:::::::::iiiiiiiii;rrv7v7v7777718@B.  ...... ..... .......................") 
+print ("............ ;8Bkriiiiii:i:::::::i:i:i:i::iiirr7r77vvv771OM@7  ... ... ..... . ....... ............") 
+print ("............ iMBOriii:iii::::::,::::::::::iiiir;rir777vrFOM@S   ... ... ... ..... . . ..... .......") 
+print ("............ :E@O7:ii:::,,,,,::::::::i:::i:iii:iiiir;7r7UOO@S  . . . . . ..... . . . ..... ........") 
+print ("............  qBMii:i:iiii;iii;iriiiiii:ir77vLJLuujv7r7rJ8MBu   . . . . . . ... . . ......... .....")
+print ("............  u@Mi:ii7vJYkGBOGNX1Lr7rrrvYSqOOMMMZEXP1Lr;iGM@J  . . . . . . . . . . . . . ..........") 
+print ("............  rBM:::r77i::i72kPX0Sj7vLJu55S1Su7:::77jLri:u@B;   ... . . . . . . . . ..... . .......")
+print ("...........   i@8::i;::L1G8M8EZEk1ri:irJJ25qN08ESj7ri77r:7B@,      . . . .   . . . ... . . . . ....")
+print (".......... .17;MO::iivBB1M@BNLEku7i:,,:rj15Zq7MB@S00Jr77ii@BYru1, .   .   . . . . . . . . ..... ...") 
+print (".......... iOqi20::::LL;:i77r7v7;i::,:ir7YLvr;rYvvvJ1Y77;:GMLvuOu    . .       . . . . ... . . ....")  
+print (".......... ikr::1::::..,::::;;r:,,:::.:i777ir;;ii;rr7r7rr:uXr;jX2       .     . . . .   . . . . ...") 
+print (".......... .v;,iL:::,,,:,:::::..,::::..irvrri;iiiii;iii7riiqL7LEi  . .   .   .       . . . ... . ..") 
+print ("........... :riYJ::::,:,,,:,,.:::::::..irr7rriiiiii:iiiiriiSNJuU      .   . .   .   . . ... . . . .") 
+print ("...........  riL7::::,,,.,.,:::::i:::..:;rrv7riiii:ii;irir:SGJ1:   .     . . .   . .   . . . . . ..")  
+print ("....... ...  :rii::::,:,,.,::::ii::,:..:;i;r2viii:iiiirrriiL2u1     . . .     .       . . . . . . .")  
+print ("............ .:::;ii:::::::::i;7;:,:,,:iirrr7ur;iiiiirrrriir71i          .   . . . .   .   . . . . ") 
+print ("....... ... . ,::ii:i::::::i;rL7;r8BS7jJYYZquuJrri;;rrr;ri7Lu7    .         .     . .     . . . . .") 
+print (".............. .,iri:i:::ii;r7r:,ijUYJ1k22J2uuLvrrrrr7iri;rvi      .     .         . . .   . . . . ") 
+print ("..... . ... ...   :i::i:iiriri:::,,.,:7rvLLLYvLv7r7r7rri;;.     .             .       . .   . . .  ") 
+print (".... ....... . .   rii:i:i:iii:::,.,,::::r77r7vv77r777;rri                   .   . .   .     . . . ") 
+print ("......... . . . .  ,rii:i:i:r7r:i;Jj2FU2qXXFUvvLY7rr7rrr7.                        .   .       .    ") 
+print ("...... . . . . . .  :7ii:i:ii7YSqGPSu2U25SFNEMOXLvrrr7r7,                      .   .     . . .   . ") 
+print ("..... . ... . . .    :7rriiirr7:i::::,:,:ir7LvYYYvv77rr:                                    . .   .") 
+print ("...... . . . . . . .  irr7r7r7ii:ir7vuSNNq2UJYvLYJLL7ri2   .                     .   .   . . . . . ") 
+print ("........... . . . .   ,uii7rr;rrr7LLU50EEXk55uuYuLL77:Pq                      . .     .   . .   . .")
+print (".... . . . ... . . .  .@X;iri;irr77YYjLv777JuUjjYLv7:uB@,  . . .             .             .     . ") 
+print ("........... . . . .   :B@Mjiiiiiiii:::;;77vrv7LvLYviuB@B,   .         .         . . .   .     . . .") 
+print (".......... . . .      ,@OBBMYriii::iir7ju5uJvLvLv77NB@BN   .                 .   . . .   .   . . . ") 
+print ("....... . .         .i0MOOMB@O17r;rrv7YjuLjU2YYvY5BB@M@r  .   .       .   .         .     . .   . .") 
+print ("                 ,vZB@BOZ8OMM@B@8NSS2SX0qNkXFFS8B@BBMBB@   . .       . .     .     . .     . . . . ") 
+print ("          .:7JS0OB@BO088MOMOMMBB@B@B@MOZ8E88MB@B@MBMMM@Y        .           . . . .   .   . .   . .") 
+print ("Fqk0GOG8GBB@@@B@BBOO8O888MOMOMMBMBB@B@B@B@B@B@BBMMMMMBO8v            .   .   . . .   . .   . . . . ")  
+print ("@B@B@B@B@B@MBOO8OOM8OOMOMOMOMGOOMMBM@B@B@B@M@BBBBMMMBOMB@Mu,              . . . . . . . . . . . . .")                                                                                                  
+print ("..................................................................................................") 
+
+print ("..................................................................................................")                                                                                                                                                      
+print ("..................................................................................................") 
+print ("..................................................................................................")
+
+print ("..@BM  :@B@@@   5B@   @B@B@  ;@BG @@  i@@ vB@  r@@@B   B@B B@   FB@   @B@B@,   i@B@Bi   BB@B@   e%a.")                                     
+print ("..B@X  .B@ @Bi  v@B   B@MGE  i@@B;B@   @B u@Y  iB@0M   @B@ PB   v@B   B@i2B@   @B1F@0   B@:M@:  O@q.")                                
+print ("  ......@B B@   YB@   @@L.   :@B@k@B   B@ XB   :@B .   B@B75@   JB@   @B  @B   B@  B@   @@qi    @@M.")                                                     
+print ("..q@j  .B@B@S   L@B   B@B@M  iB@B@@@   GBiB@   :B@B@   @B@B@B   J@B   @@  B@   @B  @B   M@B@B   &va.")                                                      
+print ("..v@i  .@B B@,  Y@@   @BMJ:  i@SkB@B   7@P@B   :@B17   @@Z@B@   JB@   @B  @B   B@  B@    .5B@r  L@i.")                                                     
+print ("..S$i  .B@ OBv  J@B   B@v.i  ;BFv@@@   :@@B@   iB@ i   @BrB@B   J@B   B@ :B@   @B r@M   Mj BBL     .")                            
+print ("..@BX  ,@B@B@:  XB@   @B@@@  r@0iB@B    @@@M   r@B@@   B@ @B@   qB@   @B@B@i   0@B@Bv   @B@B@.  0B1.")                                
+print ("..B@2  ,B@B@L   0@B   B@B@B  7BB @B@    B@@i   rB@B@   @B 2@@   Z@B   B@B@J     B@BZ    :@B@r   8@0.")
+
+print ("..................................................................................................") 
+print ("..................................................................................................")
+print ("..................................................................................................") 
+print ("--------------------------------------------------------------------------------------------------")
+
+print ("                             Realiza el Invetario de Productos                   ")
+
+print ("-------------------------------------------------------------------------------------------------")
+
+#Función para Agregar Productos
+def addProducto():
+    print("------------------------------------------------------")
+    print("1 : Agregar Productos")
+    blMenuProducto = True
+    while blMenuProducto:
+        print("------------------------------------------------------")
+        print("Seleccione Accion")
+        print("A : Agregar")
+        print("R : Regresar al menu principal")
+        menuProducto = input()
+        if(menuProducto == "A"):
+            strNombreProducto = input("Ingrese el nombre del Producto: ")
+            flValorProducto = float(input(f"Digita en valor de {strNombreProducto}: "))
+            dicProducto = {}
+            dicProducto.update({"NombreProducto":strNombreProducto})
+            dicProducto.update({"ValorProducto":flValorProducto})
+            print(dicProducto)
+            lstProductos.append(dicProducto)
+            print(lstProductos)
+        
+        else:
+            print("Gracias por la visita")
+            print ("=====================")
+            blMenuProducto = False
+
+#Función para Listar Productos
+def listProducto():
+    print("------------------------------------------------------------ \n")
+    print("Listado Del Prodcuto \n")
+    print("------------------------------------------------------------ \n")
+    
+    for p in lstProductos:
+      for (key, value) in p.items():
+         print(key, " :: ", value) 
+
+#Función para Eliminar Productos
+def delProducto():
+    print("------------------------------------------------------")
+    print("2 : Quitar Productos")
+    while True:
+        print("------------------------------------------------------")
+        print("Seleccione Accion")
+        print("E : Eliminar")
+        print("R : Regresar al menu principal")
+        menuProducto = input()
+        if(menuProducto == "E"):
+            print("Lista de Productos")
+            for p in lstProductos:
+                for (key, value) in p.items():
+                    print(key , " :: ", value )
+            print("Escribir nombre del Producto a Eliminar")
+            strNombreEliminar = input()
+            for p in lstProductos:
+                for (key, value) in p.items():
+                    if(value == strNombreEliminar):
+                        print(f"Borrar {value}???")
+                        lstProductos.remove(p)
+            print(lstProductos)
+
+        else:
+            print("Gracias por la visita")
+            print ("=====================")
+            break
+
+#Menu para Ejecutar el Inventario
+def showInventario():
+    print("------------------------------------------------------")
+    print("3 : Inventario")
+    print("------------------------------------------------------")
+    
+    print(lstProductos)
+    fltTotal=0.0
+    
+    for p in lstProductos:
+        for (key, value) in p.items():
+            # print(p['CantidadProducto'])
+            fltTprod = p['CantidadProducto'] * p['ValorProducto']
+        fltTotal += fltTprod
+    print(fltTotal)
+
+#Función para Ver el Menu
+while True:
+    print("Menu Principal: Escoge la opcion a realizar")
+    print("1 : Agregar Producto")
+    print("2 : Eliminar Producto")
+    print("3 : Inventario")
+    print("0 : Salir")
+    strMenuPrincipal = input()
+    if(strMenuPrincipal == "1"):
+        addProducto()
+    elif(strMenuPrincipal == "2"):
+        delProducto()
+    elif(strMenuPrincipal == "3"):
+        showInventario()
+    elif(strMenuPrincipal == "0"):
+        break
+    else:
+        print ("No escogio las opciones indicadas; desea salir?")
+        print ("Si : S")
+        print ("No : N")
+        opcionSalir = input()
+        print ("-----------------------------------------------")
+
+        if(opcionSalir == "S"):
+            break
+
+        0
