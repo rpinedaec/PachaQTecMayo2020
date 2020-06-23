683a332b70720e73f0e2a2e3ec8f85eb943ca333
Paola Sanchez <p.sanchezgodenzi@gmail.com>
2020-06-22 22:35:59 -0500

    C

---
+++ Semana4Hackaton/psanchez/Trabajo_S4H.py
@@ -0,0 +1,119 @@
+import os
+from time import sleep
+
+dicUsuario={}
+lstUsuario=[]
+
+class Usuario:
+    # log = utils.log("Usuario")
+
+    def __init__(self, nombre, apellido, dni, usuario, password, email):
+        self.nombre = nombre
+        self.apellido = apellido
+        self.dni = dni
+        self.usuario = usuario
+        self.password = password
+        self.email = email
+        # self.log.info("Se creo un usuario")
+    
+    def dictUsuario(self):
+        d = {
+            'nombre': self.nombre,
+            'apellido': self.apellido,
+            'dni': self.dni,
+            'usuario': self.usuario,
+            'password': self.password,
+            'email': self.email,
+        }
+        return d
+    
+    def __str__(self):
+        return """Usuario: {} \nContrasena: {}""".format(self.usuario, self.password)
+
+class Trabajador(Usuario):
+    # log = utils.log("Trabajador")
+
+    def __init__(self,nombre,apellido,dni,usuario,password,email,codTrabajador):
+        super().__init__(nombre,apellido,dni,usuario,password,email)
+        self.codTrabajador = codTrabajador
+        # self.log.info("Se creo un nuevo trabajador")
+
+class Menu:
+    def __init__(self, nombreMenu, listaOpciones):
+        self.nombreMenu = "MENU"
+        self.listaOpciones = listaOpciones
+
+    def MostrarMenu(self):
+        self.limpiarPantalla()
+        salirMenu = True
+        while(salirMenu):
+            self.limpiarPantalla()
+            print("-----------------------------------------")
+            print("        TIENDA VIRTUAL PACHAQTEC         ")
+            print("-----------------------------------------")
+            print(" * "+ self.nombreMenu +" *     ")
+            print("")
+            for (key, value) in self.listaOpciones.items():
+                print("• ",key," : [", value,"]")
+            print("•  Salir : [ 0 ]")
+            print("")
+            print("Escribe tu opción abajo")
+            opcion = 100
+            try:
+                opcion = int(input("Opción : "))
+            except:
+                contOpciones = 0
+                for (key, value) in self.listaOpciones.items():
+                    if(opcion == int(value)):
+                        contOpciones += 1
+                    if(contOpciones == 0):
+                        print("Escoge una opcion valida. \n Ingresa numeros del 0 - 9.")
+            else:
+                salirMenu = False
+        return opcion
+    
+    def limpiarPantalla(self):
+        def clear():
+            return os.system('clear')
+        clear()
+
+#Diccionario Menu Principal
+dicOpcionesMenuPrincipal = {"Usuario Nuevo": 1, "Usuario Existente": 2}
+menuPrincipal = Menu("Menu de Inicio", dicOpcionesMenuPrincipal)
+opcionMenuPrincipal = menuPrincipal.MostrarMenu()
+
+#Opciones del Menu Principal
+if (opcionMenuPrincipal == 1):
+    dicOpcionesNuevoRegistro = {"Soy Cliente": 1, "Soy Trabajador":2, "Volver": 3}
+    menuRegistro = Menu("Menu de Inicio", dicOpcionesNuevoRegistro)
+    res = menuRegistro.MostrarMenu()
+    salirNuevoRegistro = True
+    while (salirNuevoRegistro):
+        if(res == 1):
+            sleep(0.5)
+            print("")
+            print("NUEVO REGISTRO")
+            print("Ingresa los siguientes datos:")
+            print("")
+            nombre = input("Nombre :")
+            apellido = input("Apellido :")
+            dni = input("DNI :")
+            usuario = input("Escribe tu usuario :")
+            password = input("Escribe tu contrasena :")
+            email = input("Ingresa tu email :")
+            registro_nuevo = Usuario(nombre,apellido,dni,usuario,password,email)
+            lstUsuario.append(registro_nuevo.dictUsuario())
+            lstUsuario.append(registro_nuevo)
+            sleep (2)
+            print("************")
+            print("ÉXITO!")
+            print("Tu Usuario ha sido creado")
+            print("")
+            print("Tu usuario y contrasena son:")
+            for i in lstUsuario:
+                print(f"Usuario: {usuario} \n Contrasena: {password}")
+
+
+elif (opcionMenuPrincipal == 2):
+    print ("Ingresa tu usuario y contrasena:")
+
