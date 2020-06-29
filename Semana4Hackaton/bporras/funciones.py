09999f026b04d11516bcefadcd4c0517e1d1f35f
BrucePM <brucekevin45@gmail.com>
2020-06-29 08:54:32 -0500

    -Coomit

--- Semana4Hackaton/bporras/funciones.py
+++ Semana4Hackaton/bporras/funciones.py
@@ -1,20 +1,24 @@
 from io import open
 import pickle
 #funcion para buscar un objeto ya registrado segun atributo de clase y lista donde se almacena
-def search(item,identify,lst):
+
+
+def search(item, identify, lst):
     if len(lst) > 0:
-        r=0
+        r = 0
         for i in lst:
-            if getattr(i,identify) == item:
-                r +=1
-        if r > 0  :
+            if getattr(i, identify) == item:
+                r += 1
+        if r > 0:
             return True
         else:
             return False
     else:
         return False
 #funciones para añadir nuevos elementos
-def addCliente(lst,classitem):
+
+
+def addCliente(lst, classitem):
     while True:
         try:
             dni = int(input('Ingrese el dni  de la persona: '))
@@ -36,9 +40,11 @@ def addCliente(lst,classitem):
             break
         except:
             print('Error!: Ingrese un numero entero')
-    newclient = classitem(dni, nombre, apellido, edad, sexo, codcliente)    
+    newclient = classitem(dni, nombre, apellido, edad, sexo, codcliente)
     return newclient
-def addEmpleado(lst,classitem):
+
+
+def addEmpleado(lst, classitem):
     while True:
         try:
             dni = int(input('Ingrese el dni  del empleado: '))
@@ -60,9 +66,11 @@ def addEmpleado(lst,classitem):
             break
         except:
             print('Error!: Ingrese un numero entero')
-    newempleado = classitem(dni, nombre, apellido, edad, sexo, codempleado)    
+    newempleado = classitem(dni, nombre, apellido, edad, sexo, codempleado)
     return newempleado
-def addProducto(lst,classitem):
+
+
+def addProducto(lst, classitem):
     while True:
         try:
             codigo = int(input('Ingrese el codigo del producto: '))
@@ -82,41 +90,51 @@ def addProducto(lst,classitem):
             break
         except:
             print('Error!: Ingrese un numero entero')
-    newproduct = classitem(codigo, nombre, cantidad, costo)    
+    newproduct = classitem(codigo, nombre, cantidad, costo)
     return newproduct
 #funciones para eliminar elementos
+
+
 def showCliente(lst):
     while True:
         print(157*"-")
-        print(f'|{"DNI":^25}|{"Nombre":^25}|{"Apellido":^25}|{"Edad":^25}|{"Sexo":^25}|{"CodCliente":^25}|')
+        print(
+            f'|{"DNI":^25}|{"Nombre":^25}|{"Apellido":^25}|{"Edad":^25}|{"Sexo":^25}|{"CodCliente":^25}|')
         print(157*"-")
         for i in lst:
-            dni = getattr(i,"dni")
-            nombre = getattr(i,"nombre")
-            apellido = getattr(i,"apellido")
-            edad = getattr(i,"edad")
-            sexo = getattr(i,"sexo")
-            codCliente = getattr(i,"codCliente")
-            print(f'|{dni:^25}|{nombre:^25}|{apellido:^25}|{edad:^25}|{sexo:^25}|{codCliente:^25}|')
+            dni = getattr(i, "dni")
+            nombre = getattr(i, "nombre")
+            apellido = getattr(i, "apellido")
+            edad = getattr(i, "edad")
+            sexo = getattr(i, "sexo")
+            codCliente = getattr(i, "codCliente")
+            print(
+                f'|{dni:^25}|{nombre:^25}|{apellido:^25}|{edad:^25}|{sexo:^25}|{codCliente:^25}|')
         print(157*"-")
         c = input("Ingrese cualquier tecla para continuar: ")
         break
+
+
 def showEmpleado(lst):
     while True:
         print(157*"-")
-        print(f'|{"DNI":^25}|{"Nombre":^25}|{"Apellido":^25}|{"Edad":^25}|{"Sexo":^25}|{"CodEmpleado":^25}|')
+        print(
+            f'|{"DNI":^25}|{"Nombre":^25}|{"Apellido":^25}|{"Edad":^25}|{"Sexo":^25}|{"CodEmpleado":^25}|')
         print(157*"-")
         for i in lst:
-            dni = getattr(i,"dni")
-            nombre = getattr(i,"nombre")
-            apellido = getattr(i,"apellido")
-            edad = getattr(i,"edad")
-            sexo = getattr(i,"sexo")
-            codEmpleado = getattr(i,"codEmpleado")
-            print(f'|{dni:^25}|{nombre:^25}|{apellido:^25}|{edad:^25}|{sexo:^25}|{codEmpleado:^25}|')
+            dni = getattr(i, "dni")
+            nombre = getattr(i, "nombre")
+            apellido = getattr(i, "apellido")
+            edad = getattr(i, "edad")
+            sexo = getattr(i, "sexo")
+            codEmpleado = getattr(i, "codEmpleado")
+            print(
+                f'|{dni:^25}|{nombre:^25}|{apellido:^25}|{edad:^25}|{sexo:^25}|{codEmpleado:^25}|')
         print(157*"-")
         c = input("Ingrese cualquier tecla para continuar: ")
         break
+
+
 def showProducto(lst):
     while True:
         print(105*"-")
@@ -125,78 +143,93 @@ def showProducto(lst):
         print(f'|{"codProducto":^25}|{"Nombre":^25}|{"Cantidad":^25}|{"Costo":^25}|')
         print(105*"-")
         for i in lst:
-            cod = getattr(i,"codProducto")
-            nombre = getattr(i,"nombreProducto")
-            cantidad = getattr(i,"cantProducto")
-            costo = getattr(i,"costoProducto")
+            cod = getattr(i, "codProducto")
+            nombre = getattr(i, "nombreProducto")
+            cantidad = getattr(i, "cantProducto")
+            costo = getattr(i, "costoProducto")
             print(f'|{cod:^25}|{nombre:^25}|{cantidad:^25}|{costo:^25}|')
         print(105*"-")
         c = input("Ingrese cualquier tecla para continuar: ")
         break
 #Funcion para eliminar un producto
+
+
 def delProducto(lst):
     while True:
         try:
-            if len(lst) > 0 :
+            if len(lst) > 0:
                 #Muestra todos los producto
                 print(105*"-")
-                print(f'|{"codProducto":^25}|{"Nombre":^25}|{"Cantidad":^25}|{"Costo":^25}|')
+                print(
+                    f'|{"codProducto":^25}|{"Nombre":^25}|{"Cantidad":^25}|{"Costo":^25}|')
                 print(105*"-")
                 for i in lst:
-                    cod = getattr(i,"codProducto")
-                    nombre = getattr(i,"nombreProducto")
-                    cantidad = getattr(i,"cantProducto")
-                    costo = getattr(i,"costoProducto")
+                    cod = getattr(i, "codProducto")
+                    nombre = getattr(i, "nombreProducto")
+                    cantidad = getattr(i, "cantProducto")
+                    costo = getattr(i, "costoProducto")
                     print(f'|{cod:^25}|{nombre:^25}|{cantidad:^25}|{costo:^25}|')
                 print(105*"-")
                 #Elimar
-                codProducto = int(input("Ingrese el codigo del Producto a eliminar: "))
-                if search(codProducto,"codProducto",lst):
-                    strConfirmation = input(f"Esta seguro de eliminar el item {codProducto} S/N: ")
+                codProducto = int(
+                    input("Ingrese el codigo del Producto a eliminar: "))
+                if search(codProducto, "codProducto", lst):
+                    strConfirmation = input(
+                        f"Esta seguro de eliminar el item {codProducto} S/N: ")
                     if strConfirmation == "S":
-                        for i,x in enumerate(lst):
-                            if getattr(x,"codProducto") == codProducto:
+                        for i, x in enumerate(lst):
+                            if getattr(x, "codProducto") == codProducto:
                                 del lst[i]
                         print("Item eliminado correctamente")
-                        c = input("Producto eliminado, ingrese cualquier tecla para continuar: ")
+                        c = input(
+                            "Producto eliminado, ingrese cualquier tecla para continuar: ")
                         break
                     elif strConfirmation == "N":
-                        c = input("No se eliminara ningun producto, ingrese cualquier tecla para continuar: ")
+                        c = input(
+                            "No se eliminara ningun producto, ingrese cualquier tecla para continuar: ")
                         break
                     else:
                         print("Ingrese S o N")
                 else:
-                    c = input ("El producto no existe, ingrese cualquier tecla para continuar")
+                    c = input(
+                        "El producto no existe, ingrese cualquier tecla para continuar")
             else:
-                c = input("Aún no se ah registrado ningun producto, ingrese cualquier tecla para continuar")
+                c = input(
+                    "Aún no se ah registrado ningun producto, ingrese cualquier tecla para continuar")
                 break
-        except :
+        except:
             print("Error!: Ingrese un codigo de producto valido")
 #Funcion para valorizar todos los productos
+
+
 def valProducto(lst):
-    fltValTot =0.0
+    fltValTot = 0.0
     if len(lst) > 0:
         print(131*"-")
-        print(f'|{"codProducto":^25}|{"Nombre":^25}|{"Cantidad":^25}|{"Costo":^25}|{"Valor producto":^25}|')
+        print(
+            f'|{"codProducto":^25}|{"Nombre":^25}|{"Cantidad":^25}|{"Costo":^25}|{"Valor producto":^25}|')
         print(131*"-")
         for i in lst:
-            cod = getattr(i,"codProducto")
-            nombre = getattr(i,"nombreProducto")
-            cantidad = getattr(i,"cantProducto")
-            costo = getattr(i,"costoProducto")
+            cod = getattr(i, "codProducto")
+            nombre = getattr(i, "nombreProducto")
+            cantidad = getattr(i, "cantProducto")
+            costo = getattr(i, "costoProducto")
             valor = costo*cantidad
             fltValTot += costo*cantidad
             print(f'|{cod:^25}|{nombre:^25}|{cantidad:^25}|{costo:^25}|{valor:^25}|')
         print(131*"-")
-        print (f'Valorización total del inventario: S/.{fltValTot}')
+        print(f'Valorización total del inventario: S/.{fltValTot}')
         print(131*"-")
         c = input("Ingrese cualquier tecla para continuar: ")
     else:
-        c = input("Aun no se han agregado productos,ingrese cualquier tecla para continuar: ")
+        c = input(
+            "Aun no se han agregado productos,ingrese cualquier tecla para continuar: ")
 #Función de recuperación de datos
+
+
 def recovery(archivo):
-    lst=[]
-    fichero = open(f"{archivo}.pckl","ab+")
+    lst = []
+    fichero = open(f"{archivo}.pckl", "ab+")
     fichero.seek(0)
     try:
         lst = pickle.load(fichero)
@@ -208,7 +241,9 @@ def recovery(archivo):
         fichero.close()
         print(f"Se han cargado {len(lst)} {archivo}")
 #Función de guardado de datos
-def save(archivo,lst):
+
+
+def save(archivo, lst):
     fichero = open(f'{archivo}.pckl', 'wb')
     pickle.dump(lst, fichero)
     fichero.close()
