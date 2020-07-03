ebcf7cae572bbb0bd54a865f7c77411f898031fd
rpineda <roberto.pineda@pachaqtec.edu.pe>
2020-07-02 21:00:11 -0500

    Ejercicios de Clase semana 6 sesion 2

--- Semana5Hackaton/dgarcia/crudAlumno.sql
+++ Semana5Hackaton/dgarcia/crudAlumno.sql
@@ -1,6 +1,4 @@
 Select * from alumno;
-insert into alumno (nombreAlumno, edadAlumno, correoAlumno) 
-values("Denisse Garcia", "24", "denisse@pachaqtec.pe");
-update alumno 
-set  nombreAlumno = "Pepito", edadAlumno = "30",  correoAlumno = "pepito@pachaqtec.pe" where idAlumno = 1; 
+insert into alumno (nombreAlumno, edadAlumno, correoAlumno) values("Denisse Garcia", "24", "denisse@pachaqtec.pe");
+update alumno set  nombreAlumno = 'Pepito', edadAlumno = '30',  correoAlumno = 'pepito@pachaqtec.pe' where idAlumno = 2; 
 delete from alumno where idAlumno = 1;
