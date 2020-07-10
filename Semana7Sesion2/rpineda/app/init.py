cf92f6d7b95524200d7a06e486f86431b57b4cc4
rpineda <roberto.pineda@pachaqtec.edu.pe>
2020-07-09 20:03:10 -0500

    Ejercicios de clase semana 7 sesion 2

---
+++ Semana7Sesion2/rpineda/app/init.py
@@ -0,0 +1,22 @@
+import conexion
+
+data = {"nombreTipoPago": "Contado"}
+conn = conexion.conexionBDD(4)
+datas = [
+    {"nombreTipoPago": "Credito"}, 
+    {"nombreTipoPago": "Tarjeta de Credito"}]
+#coll = conn.insertarMongo("tipopago",data)
+#coll = conn.insertarResgistros("tipopago", datas)
+data = {"nombreTipoPago": "Credito"}
+#coll = conn.leerRegistro("tipopago",data)
+coll = conn.leerRegistros("tipopago",data)
+
+for obj in coll:
+    print(obj)
+
+dataActualizar = {"nombreTipoPago": "Credito 180 dias"}
+coll = conn.actualizarResgistro("tipopago",data,dataActualizar)
+print (coll)
+data = {}
+coll = conn.leerRegistros("tipopago",data)
+print (coll)
