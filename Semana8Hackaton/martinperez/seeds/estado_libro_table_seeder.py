f9403c5e21bf4a7c2c66eaae63d358f557c36067
martin perez <perez_13lo@hotmail.com>
2020-07-19 22:19:27 -0500

    MartinPerez - Semana8Hackaton

---
+++ Semana8Hackaton/martinperez/seeds/estado_libro_table_seeder.py
@@ -0,0 +1,19 @@
+from orator.seeds import Seeder
+
+
+class EstadoLibroTableSeeder(Seeder):
+
+    def run(self):
+        """
+        Run the database seeds.
+        """
+        self.db.table('estadolibro').insert({
+            'descripcion' : 'Disponible'
+        })
+        self.db.table('estadolibro').insert({
+            'descripcion' : 'Reservado'
+        })
+        self.db.table('estadolibro').insert({
+            'descripcion' : 'Prestado'
+        })
+
