703c0354fb3f6bca7026278af00b8076f632044c
JWilian <wilianders.uni@gmail.com>
2020-07-26 22:34:47 -0500

    modulo cajero creado

--- Semana9Hackaton/jquispe/django/minimarket/cajero/models.py
+++ Semana9Hackaton/jquispe/django/minimarket/cajero/models.py
@@ -1,3 +1,27 @@
 from django.db import models
-
+from almacen.models import Producto
 # Create your models here.
+
+class Cliente(models.Model):
+	nombreCliente = models.CharField(max_length=200)
+	dniCliente = models.CharField(max_length=8)
+	direccionCliente = models.CharField(max_length=200)
+
+class Empresa(models.Model):
+	rucEmpresa = models.CharField(max_length=11)
+	direccionEmpresa = models.CharField(max_length=200)
+
+class FacturaCabecera(models.Model):
+	fechaFacturaCabecera = models.DateTimeField()
+	igvFacturaCabecera = models.DecimalField(max_digits=10, decimal_places=2)
+	subtotalFacturaCabecera = models.DecimalField(max_digits=10, decimal_places=2)
+	totalFacturaCabecera = models.DecimalField(max_digits=10, decimal_places=2)
+	estadoFactura = models.CharField(max_length=20)
+	idCliente = models.ForeignKey(Cliente, on_delete=models.CASCADE)
+	idEmpresa = models.ForeignKey(Empresa, on_delete=models.CASCADE)
+
+class facturaDetalle(models.Model):
+	cantFactDetalle = models.IntegerField(default=0)
+	valorFactDetalle = models.DecimalField(max_digits=10,decimal_places=2)
+	idFacturaCabecera = models.ForeignKey(FacturaCabecera, on_delete=models.CASCADE)
+	idProductos = models.ForeignKey(Producto, on_delete=models.CASCADE)
--- Semana9Hackaton/jquispe/django/minimarket/minimarket/settings.py
+++ Semana9Hackaton/jquispe/django/minimarket/minimarket/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'cajero.apps.CajeroConfig',
     'almacen.apps.AlmacenConfig',
     'django.contrib.admin',
     'django.contrib.auth',
