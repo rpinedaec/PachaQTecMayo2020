d0af798f230b4b6a36bf2697c463dd7dc045506c
JWilian <wilianders.uni@gmail.com>
2020-07-26 23:51:05 -0500

    final

--- Semana9Hackaton/jquispe/django/minimarket/administrador/models.py
+++ Semana9Hackaton/jquispe/django/minimarket/administrador/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+
+class VentaDiaria(models.Model):
+    venta = models.DecimalField(max_digits=10, decimal_places=2)
+    fecha = models.DateTimeField()
--- Semana9Hackaton/jquispe/django/minimarket/minimarket/settings.py
+++ Semana9Hackaton/jquispe/django/minimarket/minimarket/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'administrador.apps.AdministradorConfig',
     'cajero.apps.CajeroConfig',
     'almacen.apps.AlmacenConfig',
     'django.contrib.admin',
