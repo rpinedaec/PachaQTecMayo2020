55b939b39b422d4d2c52fa4524f0435ab08c7771
Lorena Oyola <66941019+Lorena1520041@users.noreply.github.com>
2020-07-29 02:34:16 -0500

    Add files via upload

---
+++ Semana9Hackaton/loyola/django/djenv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\pachaqtec\junio2020\proyectos\hackatonsemana9\loyola\django\djenv\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
