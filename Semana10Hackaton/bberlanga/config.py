f2a43ed2973a3a46e7725e88116922cac7a9a758
braulioB berlanga <braulioberlanga@gmail.com>
2020-08-10 22:01:12 -0500

    commit

--- Semana10Hackaton/bberlanga/config.py
+++ Semana10Hackaton/bberlanga/config.py
@@ -3,5 +3,5 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
-    SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:3179billace@localhost/10Hackaton'
+    SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:3179billace@localhost/Hackaton'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
