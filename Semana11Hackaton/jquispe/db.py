15a89eae4d08dc87e64efc186eff392ef460c764
JWilian <wilianders.uni@gmail.com>
2020-08-10 23:54:14 -0500

    Semana-Hackathon

---
+++ Semana11Hackaton/jquispe/db.py
@@ -0,0 +1,10 @@
+DATABASES = {
+    'postgres': {
+        'driver': 'postgres',
+        'host': 'localhost',
+        'database': 'hackatons11',
+        'user': 'postgres',
+        'password': 'uni20100420f',
+        'prefix': ''
+    }
+}
