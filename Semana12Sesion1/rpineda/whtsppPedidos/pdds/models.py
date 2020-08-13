2fc5640de5b4880f033ba102d0f2135533403e70
rpineda (Pineda Lopez <rpineda@zegelipae.edu.pe>
2020-08-12 21:00:05 -0500

    Semana 12 sesion 2 clases

--- Semana12Sesion1/rpineda/whtsppPedidos/pdds/models.py
+++ Semana12Sesion1/rpineda/whtsppPedidos/pdds/models.py
@@ -1,3 +1,34 @@
 from django.db import models
 
 # Create your models here.
+class tipoClientes(models.Model):
+    ACTIVO = 'AC'
+    INACTIVO = 'IA'
+    descripcion = models.CharField(max_length=200)
+    isActivoEnum = ((ACTIVO,'Activo'),(INACTIVO,'Inactivo'))
+    isActivo = models.CharField(
+        max_length=2,
+        choices=isActivoEnum,
+        default=ACTIVO,
+    )
+
+class tipoDocumentos(models.Model):
+    ACTIVO = 'AC'
+    INACTIVO = 'IA'
+    descripcion = models.CharField(max_length=200)
+    isActivoEnum = ((ACTIVO,'Activo'),(INACTIVO,'Inactivo'))
+    isActivo = models.CharField(
+        max_length=2,
+        choices=isActivoEnum,
+        default=ACTIVO,
+    )
+class tipoProductos(models.Model):
+    ACTIVO = 'AC'
+    INACTIVO = 'IA'
+    descripcion = models.CharField(max_length=200)
+    isActivoEnum = ((ACTIVO,'Activo'),(INACTIVO,'Inactivo'))
+    isActivo = models.CharField(
+        max_length=2,
+        choices=isActivoEnum,
+        default=ACTIVO,
+    )
