82f835accd577c12e898c454825f6d32c474cb06
rpineda (Pineda Lopez <rpineda@zegelipae.edu.pe>
2020-09-05 12:43:53 -0500

    Semana 15 Clases

--- Semana15Sesion1/rpineda/pedidos/views.py
+++ Semana15Sesion1/rpineda/pedidos/views.py
@@ -1,14 +1,16 @@
 from rest_framework import viewsets
-from rest_framework import permissions
 from pedidos.serializers import TipoClienteSerializer, TipoProductoSerializer
 from pedidos.models import tipoCliente, tipoProducto
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import filters
 
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
 
 class TipoClienteViewSet(viewsets.ModelViewSet):
     serializer_class = TipoClienteSerializer
-    permission_classes = [permissions.IsAuthenticated]
+    permission_classes = [IsAuthenticated,]
     def get_queryset(self):
         queryset = tipoCliente.objects.all()
         desc = self.request.query_params.get('desc',None)
@@ -19,7 +21,7 @@ class TipoClienteViewSet(viewsets.ModelViewSet):
 class TipoProductoViewSet(viewsets.ModelViewSet):
     queryset = tipoProducto.objects.all()
     serializer_class = TipoProductoSerializer
-    permission_classes = [permissions.IsAuthenticated]
+    permission_classes = [IsAuthenticated,]
     #filter_backends = [DjangoFilterBackend]
     filter_backends = [filters.SearchFilter]
     #search_fields = ['username', 'email']
