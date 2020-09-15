6e30dd243ab2979d09e3ba0896883caa7d627c5c
Bryan Arias <bryan.arias@gmail.com>
2020-09-14 23:01:30 -0500

    hackaton 16

--- Semana15Hackaton/Bryan Arias/coches/models.py
+++ Semana15Hackaton/Bryan Arias/coches/models.py
@@ -4,13 +4,22 @@ from django.db import models
 
 class Modelo(models.Model):
     descripcion = models.CharField(max_length=100)
+    
+    def __str__(self):
+        return self.descripcion
 
 class Marca(models.Model):
     descripcion = models.CharField(max_length=100)
     modelo = models.ForeignKey(Modelo, on_delete=models.CASCADE)
 
+    def __str__(self):
+        return self.descripcion
+
 class Auto(models.Model):    
     descripcion = models.CharField(max_length=100)
     marca = models.ForeignKey(Marca, on_delete=models.CASCADE)
     color = models.CharField(max_length = 50)
+
+    def __str__(self):
+        return self.descripcion
     
