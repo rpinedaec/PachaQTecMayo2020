2999d35789667f91f5d9539f7af57c35e30e8300
Denisse Garcia <garciasayand@gmail.com>
2020-09-15 01:32:41 -0500

    d

--- Semana16Hackaton/dgarcia copy/musicapp/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class MusicappConfig(AppConfig):
-    name = 'musicapp'
--- Semana16Hackaton/dgarcia/musicapp/serializers.py
+++
@@ -1,9 +0,0 @@
-from rest_framework import serializers
-
-from musicapp.models import Artista
-
-
-class ArtistaSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Artista
-        fields = '__all__'
